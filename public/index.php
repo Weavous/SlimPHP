4e49a71b638970341393ffda44d531774cbbd879
Wesley Flôres <wesleyfloresterres@gmail.com>
2021-01-03 13:26:23 -0300

    index.php

--- public/index.php
+++ public/index.php
@@ -1,15 +1,15 @@
 <?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
 require __DIR__ . '/../vendor/autoload.php';
 
-// Create and configure Slim app
-$config = ['settings' => [
-    'addContentLengthHeader' => false,
-]];
-$app = new \Slim\App($config);
+$app = new \Slim\App;
 
 // Define app routes
-$app->get('/hello/{name}', function ($request, $response, $args) {
-    return $response->write("Hello " . $args['name']);
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    return $response->getBody()->write("Hello, " . $args['name']);
 });
 
 // Run app
